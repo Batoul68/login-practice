493e31db199541981c8caba03ad3952451e23c30
Batoul <batoulalso68@gmail.com>
2026-08-25 19:59:39 -0400

    protected routes

--- login_practice/src/components/App/App.jsx
+++ login_practice/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import useToken from './useToken.js';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute.jsx';
 import Dashboard from '../Dashboard/Dashboard.jsx';
 import Preferences from '../Preferences/Preferences.jsx';
 import LoginPage from '../LoginPage/LoginPage.jsx';
@@ -10,17 +11,28 @@ function App() {
 
   const { token, setToken } = useToken();
 
-  if (!token) {
-    return <LoginPage setToken={setToken} />
-  }
-
   return(
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/preferences" element={<Preferences />}/>
+          <Route path="/login" element={<LoginPage setToken={setToken} />} />
+          <Route 
+            path="/dashboard" 
+            element={
+              <ProtectedRoute token={token}>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route 
+            path="/preferences" 
+            element={
+              <ProtectedRoute token={token}>
+                <Preferences />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
